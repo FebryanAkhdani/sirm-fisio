dd55987a05c236f50ebf8c892ff249ff53e99b0b
Febryan Akhdani <akhdanifebryan@gmail.com>
2024-12-21 20:55:55 +0700

    add UserSeeder

--- app/Database/Seeds/UserSeeder.php
+++ app/Database/Seeds/UserSeeder.php
@@ -3,6 +3,7 @@
 namespace App\Database\Seeds;
 
 use CodeIgniter\Database\Seeder;
+use CodeIgniter\Shield\Entities\User;
 use CodeIgniter\Shield\Models\UserModel;
 
 class UserSeeder extends Seeder
@@ -11,20 +12,23 @@ class UserSeeder extends Seeder
     {
         $users = model(UserModel::class);
 
-        // Tambah user dengan password yang di-hash otomatis oleh UserModel
-        // $users->save([
-        //     'email'    => 'user@example.com',
-        //     'username' => 'user123',
-        //     'password' => 'password123',  // Password akan di-hash oleh Shield
-        // ]);
+        $users = auth()->getProvider();
 
-        // Tambah admin
-        $users->save([
-            'email'    => 'admin@example.com',
-            'username' => 'admin123',
-            'password' => 'adminpassword',
-            'active'   => 1,  // Opsional: aktifkan user
-            'roles'    => ['admin']  // Menambahkan role admin (disesuaikan dengan role Shield)
+        $user = new User([
+            'username' => 'Admin',
+            'email'    => 'admin@email.com',
+            'password' => '123456789ccd',
         ]);
+
+        $users->save($user);
+
+        // To get the complete user object with ID, we need to get from the database
+        $user = $users->findById($users->getInsertID());
+
+        // Add to default group
+        $users->addToAdminGroup($user);
+
+        // Aktifkan user
+        $user->activate();
     }
 }
